6bfc11bf7ff7be2d8ccea771a4b53efea8aa553c
Shirley Kabir <shirleykabir@gmail.com>
2018-08-26 12:36:19 -0400

    fix apply now

--- js/main.js
+++ js/main.js
@@ -6,11 +6,12 @@ var navigationLinks = {
     "Aircraft": "/web-draft/aircraft.html",
     "Recruitment": "/web-draft/recruitment.html",
     "Sponsors": "/web-draft/sponsors.html",
-    "Contact Us": "/web-draft/contact-us.html"
+    "Contact Us": "/web-draft/contact-us.html",
+    "Apply Now!": "/web-draft/apply-now.html"
 };
 
 function convertToClassName(url) {
-    return url.substring(1, url.length - 5);
+    return url.substring(11,11+(url.length-16));
 }
 
 var navigationContainer = document.getElementById("navigation");
@@ -40,8 +41,8 @@ var activeLink;
 if (path == "/web-draft/index.html" || path == "/web-draft/the-team.html" || path == "/web-draft/aircraft.html" || path == "/web-draft/recruitment.html" || path == "/web-draft/sponsors.html" || path == "/web-draft/contact-us.html") {
     activeLink = "." + convertToClassName(path);
 } else if (path == "/" || path=="/web-draft/") {
-    activeLink = "." + convertToClassName("/index.html");
+    activeLink = "." + convertToClassName("/web-draft/index.html");
 } else {
-    activeLink = "." + convertToClassName("/the-team.html");
+    activeLink = "." + convertToClassName("/web-draft/the-team.html");
 }
 $(activeLink).append("<svg class='hoverLine'><line x1='0' y1='35' x2='100' y2='20' stroke='#E8000060' stroke-width='7' /></svg>");
